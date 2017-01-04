146574558030152f9d964cd7c284dc64c8d9246c
Ivan Šokčević <ivan.sokcevic@mbit.io>
2017-01-04 13:49:24 +0100

    Add participant type model

---
+++ db/migrate/20170104124858_create_participant_types.rb
@@ -0,0 +1,9 @@
+class CreateParticipantTypes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :participant_types do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
