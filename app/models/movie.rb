7359b006178917493b284a4c1020fe93831ada50
Ivan Šokčević <ivan.sokcevic@mbit.io>
2017-01-04 14:53:01 +0100

    Add relationships to movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,11 @@
 class Movie < ApplicationRecord
+  has_many :roles
+
+  has_many :role_types, through: :roles
+  has_many :departments, through: :role_types
+
+  has_many :participants, through: :roles
+
+  has_many :authorships
+  has_many :authors, through: :authorships, class_name: :participant
 end
