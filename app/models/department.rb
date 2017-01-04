87a3d3d38326d162341062cccb51ddf9d0903504
Ivan Šokčević <ivan.sokcevic@mbit.io>
2017-01-04 14:21:50 +0100

    Add relations to department model

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,2 +1,6 @@
 class Department < ApplicationRecord
+  has_many :role_types
+  has_many :roles, through: :role_types
+  has_many :movies, through: :roles
+  has_many :participants, through: :roles
 end
