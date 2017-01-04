2d55d90f6007b0dabd1c15b0f3732591651e16f8
Ivan Šokčević <ivan.sokcevic@mbit.io>
2017-01-04 14:28:22 +0100

    Add relations to participant model

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,3 +1,8 @@
 class Participant < ApplicationRecord
   belongs_to :participant_type
+
+  has_many :roles
+  has_many :movies, through: :roles
+  has_many :role_types, through: :roles
+  has_many :departments, through: :role_types
 end
