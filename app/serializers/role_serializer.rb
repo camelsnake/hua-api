9f9edbc3db28dbc14c75228206a1ab7be7af02c3
Ivan Šokčević <ivan.sokcevic@mbit.io>
2017-01-05 17:23:20 +0100

    Add role type to role payload

--- app/serializers/role_serializer.rb
+++ app/serializers/role_serializer.rb
@@ -1,8 +1,12 @@
 class RoleSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :role_type
 
   has_one :movie
   has_one :participant
   has_one :department
+
+  def role_type
+    object.role_type.name
+  end
 
 end
